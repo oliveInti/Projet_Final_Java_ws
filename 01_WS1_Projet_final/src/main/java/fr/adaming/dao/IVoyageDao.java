5f22c44c40ef0aea5b85120a94ef9f18a696a31c
oliveInti <31440866+oliveInti@users.noreply.github.com>
2017-08-29 17:27:34 +0200

    commit : rudy : 29/08 17:27 : dao Voyage

---
+++ 01_WS1_Projet_final/src/main/java/fr/adaming/dao/IVoyageDao.java
@@ -0,0 +1,48 @@
+package fr.adaming.dao;
+
+import java.util.List;
+
+import fr.adaming.model.Voyage;
+
+/**
+ * Extention de l'interface Générique
+ * Contient les méthodes CRUD
+ * + [rien pour le moment]
+ * @author INTI2017FORMATION
+ *
+ */
+public interface IVoyageDao extends IGeneriqueDao<Voyage> {
+	/**
+	 * Méthode appelant la liste des voyages en BDD
+	 * @return Liste des Voyages proposé par l'agence
+	 */
+	public List<Voyage> recupererTout();
+
+	/**
+	 * Méthode permettant de récupérer le détail d'un voyage
+	 * @param identifiant de l'objet Voyage à récupérer en BDD
+	 * @return un objet Voyage
+	 */
+	public Voyage recupererParId(int idVoyage);
+
+	/**
+	 * Méthode permettant de créer un voyage et de l'injecter en BDD
+	 * @param l'objet Voyage à injecter en BDD
+	 */
+	public void creer(Voyage voyage);
+
+	/**
+	 * Méthode permettant de supprimer un voyage en BDD
+	 * @param objet Voyage à supprimer de la BDD
+	 */
+	public void supprimer(Voyage voyage);
+
+	/**
+	 * Méthode permettant de mettre à jour un objet Voyage
+	 * présent en BDD
+	 * @param objet Voyage à modifier
+	 * @return objet Voyage modifier
+	 */
+	public Voyage modifier(Voyage voyage);
+
+}
---
+++ 01_WS1_Projet_final/src/main/java/fr/adaming/dao/VoyageDaoImpl.java
@@ -0,0 +1,69 @@
+package fr.adaming.dao;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import fr.adaming.model.Voyage;
+
+/**
+ * 
+ * @author INTI2017FORMATION
+ *
+ */
+@Repository
+public class VoyageDaoImpl implements IVoyageDao {
+
+	@Autowired
+	private SessionFactory sf;
+
+	@Override
+	public List<Voyage> recupererTout() {
+		// ouvrir une session (bus de données -> bud)
+		Session s = sf.getCurrentSession();
+		// la requête HQL
+		String req = "FROM Voyage";
+
+		Query query = s.createQuery(req);
+
+		// envoie de la requête et récupération du résultat
+		@SuppressWarnings("unchecked")
+		List<Voyage> liste = query.list();
+
+		return liste;
+
+	}
+
+	@Override
+	public Voyage recupererParId(int idVoyage) {
+		Session s = sf.getCurrentSession();
+		Voyage varVoyage = (Voyage) s.get(Voyage.class, idVoyage);
+		return varVoyage;
+	}
+
+	@Override
+	public void creer(Voyage voyage) {
+		// ouvrir une session (bus de données -> bdd)
+		Session s = sf.getCurrentSession();
+		// ajouter l'employé dans le context Hibernate
+		s.save(voyage);
+	}
+
+	@Override
+	public void supprimer(Voyage voyage) {
+		Session s = sf.getCurrentSession();
+		s.delete(voyage);
+	}
+
+	@Override
+	public Voyage modifier(Voyage voyage) {
+		Session s = sf.getCurrentSession();
+		s.saveOrUpdate(voyage);
+		return voyage;
+	}
+
+}
