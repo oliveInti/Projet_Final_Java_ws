ebc704f07ae3a70412d8d1c2bdd17ad9728d92fe
oliveInti <31440866+oliveInti@users.noreply.github.com>
2017-08-30 12:03:31 +0200

    commit webservice - Noémie

---
+++ 01_WS1_Projet_final/src/main/java/fr/adaming/restControllers/CommandeRestController.java
@@ -0,0 +1,57 @@
+package fr.adaming.restControllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.adaming.model.Commande;
+import fr.adaming.service.IGeneriqueService;
+
+/**
+ * Définition des méthodes du web service Voyage avec Spring MVC
+ * 
+ * @author projet blue
+ *
+ */
+
+@RestController
+@RequestMapping("/voyages")
+public class CommandeRestController {
+
+	@Autowired
+	private IGeneriqueService<Commande> commandeService;
+
+	public void setCommandeService(IGeneriqueService<Commande> commandeService) {
+		this.commandeService = commandeService;
+	}
+	
+	@RequestMapping(value = "/listeCommande", method = RequestMethod.GET, produces = "application/json")
+	public List<Commande> recupererTout() {
+		return commandeService.recupererTout();
+	}
+
+	@RequestMapping(value = "/commande/{pId}", method = RequestMethod.GET, produces = "application/json")
+	public Commande recupererParId(@PathVariable("pId") int id) {
+		return commandeService.recupererParId(id);
+	}
+	
+	@RequestMapping(value="/ajout", method=RequestMethod.POST, consumes="application/json")
+	public void ajouterCommande(@RequestBody Commande c) {
+		commandeService.creer(c);
+	}
+	
+	@RequestMapping(value="/modif", method=RequestMethod.PUT, consumes="application/json")
+	public void modifierCommande(@RequestBody Commande c){
+		commandeService.modifier(c);
+	}
+
+	@RequestMapping(value="/supp", method=RequestMethod.DELETE, consumes="application/json")
+	public void supprimerCcommande(@RequestBody Commande c) {
+		commandeService.supprimer(c);
+	}
+}
---
+++ 01_WS1_Projet_final/src/main/java/fr/adaming/restControllers/VoyageRestController.java
@@ -0,0 +1,57 @@
+package fr.adaming.restControllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.adaming.model.Voyage;
+import fr.adaming.service.IGeneriqueService;
+
+/**
+ * Définition des méthodes du web service Voyage avec Spring MVC
+ * 
+ * @author projet blue
+ *
+ */
+
+@RestController
+@RequestMapping("/voyages")
+public class VoyageRestController {
+
+	@Autowired
+	private IGeneriqueService<Voyage> voyageService;
+
+	public void setVoyageService(IGeneriqueService<Voyage> voyageService) {
+		this.voyageService = voyageService;
+	}
+	
+	@RequestMapping(value = "/listeVoyage", method = RequestMethod.GET, produces = "application/json")
+	public List<Voyage> recupererTout() {
+		return voyageService.recupererTout();
+	}
+
+	@RequestMapping(value = "/voyage/{pId}", method = RequestMethod.GET, produces = "application/json")
+	public Voyage recupererParId(@PathVariable("pId") int id) {
+		return voyageService.recupererParId(id);
+	}
+	
+	@RequestMapping(value="/ajout", method=RequestMethod.POST, consumes="application/json")
+	public void ajouterVoyage(@RequestBody Voyage v) {
+		voyageService.creer(v);
+	}
+	
+	@RequestMapping(value="/modif", method=RequestMethod.PUT, consumes="application/json")
+	public void modifierVoyage(@RequestBody Voyage v){
+		voyageService.modifier(v);
+	}
+
+	@RequestMapping(value="/supp", method=RequestMethod.DELETE, consumes="application/json")
+	public void supprimerVoyage(@RequestBody Voyage v) {
+		voyageService.supprimer(v);
+	}
+}
